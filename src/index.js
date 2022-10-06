e2666d3ee6127c56de921eafbe8431877f64609d
CBVeLuMe <cbvelume@hotmail.com>
2022-10-06 15:53:08 -0400

    Add redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(
+root.render(<Provider store={store}>
     <BrowserRouter>
         <App/>
     </BrowserRouter>
-);
+</Provider>);
